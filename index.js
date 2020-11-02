1ab596cf2569274e00f5fa0bc11b195e528b20a5
IziTheOnjo <73803372+IziTheOnjo@users.noreply.github.com>
2020-11-02 13:09:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,18 @@ var command = messageArray[0];
         return message.channel.send("Doner");
     }
 
+    if(command === `${prefix}donerkebab`) {
+
+        var botEmbed = new discord.MessageEmbed()
+            .setTitle("OOOOOOOOOOOOOOW")
+            .setDescription("Ey Lachmagoen hoofd Je kan maar een Gebruiken Hé")
+            .setColor("#fc1c03")
+            .setImage("https://cdn.drawception.com/drawings/611640/BZWcJsYnvj.png")
+            
+        return;
+
+    }
+
 });
 
 client.login(process.env.token);
